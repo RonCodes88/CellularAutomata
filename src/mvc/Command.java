98adc0673afc0677d1887566661faeb6419c4c25
Caden King <ivashinking@gmail.com>
2024-03-05 15:26:41 -0800

    finished Command.java mvc framework draft

--- src/mvc/Command.java
+++ src/mvc/Command.java
@@ -3,8 +3,14 @@ package mvc;
 /*
 Edits:
    Ronald 3/4/24: created file
-
+   Caden 3/5/24: implemented class for mvc framework
 */
 
-public class Command {
+public abstract class Command {
+    public Model model;
+    public Command(Model model)
+    {
+        this.model = model;
+    }
+    public abstract void execute();
 }
