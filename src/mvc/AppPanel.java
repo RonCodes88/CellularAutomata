ce54120090a0c160cf0259afeecca7df10c82b2d
yogichip <123456chipalkatti@gmail.com>
2024-03-06 19:08:46 -0800

    changes made 3/6

--- src/mvc/AppPanel.java
+++ src/mvc/AppPanel.java
@@ -1,10 +1,55 @@
 package mvc;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 /*
 Edits:
    Ronald 3/4/24: created file
+   Yogi 3/6/24: created constructor, inner ControlPanel class and import statements
 
 */
 
-public class AppPanel {
+public class AppPanel extends JPanel implements ActionEventListener{
+    private ControlPanel controls;
+    private Model model;
+    private View view;
+
+    public AppPanel(){
+        model = new Model();
+        view = new View();
+        controls = new ControlPanel();
+        this.setLayout((new GridLayout(1, 2)));
+        this.add(controls);
+        this.add(view);
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container cp = frame.getContentPane();
+        cp.add(this);
+        frame.setJMenuBar(this.createMenuBar());
+        frame.setTitle("Cellular Automata");
+        frame.setSize(700, 500);
+        frame.setVisible(true);
+    }
+
+    protected JMenuBar createMenuBar() {
+        JMenuBar result = new JMenuBar();
+        JMenu fileMenu = Utilities.makeMenu("File", new String[]{"New", "Save", "Open", "Quit"}, this);
+        result.add(fileMenu);
+        JMenu editMenu = Utilities.makeMenu("Edit", new String[]{"RUN1", "RUN50", "POPULATE", "CLEAR"}, this);
+        result.add(editMenu);
+        JMenu helpMenu = Utilities.makeMenu("Help", new String[]{"About", "Help"}, this);
+        result.add(helpMenu);
+        return result;
+    }
+    private class ControlPanel extends JPanel{
+
+    }
+
 }
